5084a08c4b63428a9ee7d7ff877c0b8d37922ee2
Balaji J <j.balaji2468@gmail.com>
2026-04-30 14:00:23 +0530

    fix: implemented non CGO splitter
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/parser/splitter_test.go
+++ internal/parser/splitter_test.go
@@ -5,102 +5,39 @@ import (
 
 	"github.com/balaji01-4d/pgxcli/internal/parser"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestSplitSqlStatement_Empty(t *testing.T) {
-	assertSplit(t, "", nil)
-}
+func TestSplitSqlStatements(t *testing.T) {
+	tests := []struct {
+		name string
+		sql  string
+		want []string
+	}{
+		{"Empty", "", []string{""}},
+		{"WhitespaceOnly", "   \n\t  ", []string{"   \n\t  "}},
+		{"SingleStatement", "SELECT 1;", []string{"SELECT 1;"}},
+		{"NoTrailingSemicolon", "SELECT 1", []string{"SELECT 1"}},
+		{"MultipleStatements", "SELECT 1; SELECT 2; SELECT 3;", []string{"SELECT 1;", "SELECT 2;", "SELECT 3;"}},
+		{"LeadingAndTrailingSemicolons", ";SELECT 1;;", []string{";", "SELECT 1;", ";"}},
+		{"EmptyStatementsBetweenValids", "SELECT 1;;SELECT 2;", []string{"SELECT 1;", ";", "SELECT 2;"}},
+		{"MultilineStatements", "SELECT\n1;\nSELECT\n2;", []string{"SELECT\n1;", "SELECT\n2;"}},
+		{"SemicolonInStringLiteral", "SELECT 'value;with;semicolons';", []string{"SELECT 'value;with;semicolons';"}},
+		{"SemicolonInDoubleQuotedIdentifier", `SELECT "col;name" FROM t;`, []string{`SELECT "col;name" FROM t;`}},
+		{"DoubleSingleQuoteEscaping", "SELECT 'it''s;fine';", []string{"SELECT 'it''s;fine';"}},
+		{"MultilineComment", "SELECT 1; /* comment; inside */ SELECT 2;", []string{"SELECT 1;", "/* comment; inside */ SELECT 2;"}},
+		{"NestedCommentLikeInput", "SELECT 1 /* outer /* inner */ still */; SELECT 2;", []string{"SELECT 1 /* outer /* inner */ still */;", "SELECT 2;"}},
+		{"LineCommentAtEndNoSemicolon", "SELECT 1 -- comment", []string{"SELECT 1 -- comment"}},
+		{"MixedCommentStyles", "SELECT 1; -- c1\n/* c2; */ SELECT 2;", []string{"SELECT 1;", "-- c1\n/* c2; */ SELECT 2;"}},
+		{"SemicolonInDollarQuotedString", "SELECT $$abc;def$$;", []string{"SELECT $$abc;def$$;"}},
+		{"Unicode", "SELECT '你好;世界';", []string{"SELECT '你好;世界';"}},
+		{"UnclosedString", "SELECT 'abc;", []string{"SELECT 'abc;"}},
+		{"UnclosedComment", "SELECT 1; /* unclosed", []string{"SELECT 1;", "/* unclosed"}},
+	}
 
-func TestSplitSqlStatement_WhitespaceOnly(t *testing.T) {
-	assertSplit(t, "   \n\t  ", nil)
-}
-
-func TestSplitSqlStatement_SingleStatement(t *testing.T) {
-	assertSplit(t, "SELECT 1;", []string{"SELECT 1"})
-}
-
-func TestSplitSqlStatement_NoTrailingSemicolon(t *testing.T) {
-	assertSplit(t, "SELECT 1", []string{"SELECT 1"})
-}
-
-func TestSplitSqlStatement_MultipleStatements(t *testing.T) {
-	assertSplit(t, "SELECT 1; SELECT 2; SELECT 3;", []string{"SELECT 1", "SELECT 2", "SELECT 3"})
-}
-
-func TestSplitSqlStatement_LeadingAndTrailingSemicolons(t *testing.T) {
-	assertSplit(t, ";SELECT 1;;", []string{"SELECT 1"})
-}
-
-func TestSplitSqlStatement_EmptyStatementsBetweenValids(t *testing.T) {
-	assertSplit(t, "SELECT 1;;SELECT 2;", []string{"SELECT 1", "SELECT 2"})
-}
-
-func TestSplitSqlStatement_MultilineStatements(t *testing.T) {
-	assertSplit(t, "SELECT\n1;\nSELECT\n2;", []string{"SELECT\n1", "SELECT\n2"})
-}
-
-func TestSplitSqlStatement_SemicolonInStringLiteral(t *testing.T) {
-	assertSplit(t, "SELECT 'value;with;semicolons';", []string{"SELECT 'value;with;semicolons'"})
-}
-
-func TestSplitSqlStatement_SemicolonInDoubleQuotedIdentifier(t *testing.T) {
-	assertSplit(t, `SELECT "col;name" FROM t;`, []string{`SELECT "col;name" FROM t`})
-}
-
-func TestSplitSqlStatement_DoubleSingleQuoteEscaping(t *testing.T) {
-	assertSplit(t, "SELECT 'it''s;fine';", []string{"SELECT 'it''s;fine'"})
-}
-
-func TestSplitSqlStatement_BackslashEscapedQuoteErrors(t *testing.T) {
-	assertSplitErr(t, "SELECT 'it\\'s;fine';")
-}
-
-func TestSplitSqlStatement_MultilineComment(t *testing.T) {
-	assertSplit(t, "SELECT 1; /* comment; inside */ SELECT 2;", []string{"SELECT 1", "/* comment; inside */ SELECT 2"})
-}
-
-func TestSplitSqlStatement_NestedCommentLikeInput(t *testing.T) {
-	assertSplit(t, "SELECT 1 /* outer /* inner */ still */; SELECT 2;", []string{"SELECT 1 /* outer /* inner */ still */", "SELECT 2"})
-}
-
-func TestSplitSqlStatement_LineCommentAtEndNoSemicolon(t *testing.T) {
-	assertSplit(t, "SELECT 1 -- comment", []string{"SELECT 1 -- comment"})
-}
-
-func TestSplitSqlStatement_MixedCommentStyles(t *testing.T) {
-	assertSplit(t, "SELECT 1; -- c1\n/* c2; */ SELECT 2;", []string{"SELECT 1", "-- c1\n/* c2; */ SELECT 2"})
-}
-
-func TestSplitSqlStatement_SemicolonInDollarQuotedString(t *testing.T) {
-	assertSplit(t, "SELECT $$abc;def$$;", []string{"SELECT $$abc;def$$"})
-}
-
-func TestSplitSqlStatement_BeginEndBlockErrors(t *testing.T) {
-	assertSplitErr(t, "BEGIN SELECT 1; SELECT 2; END;")
-}
-
-func TestSplitSqlStatement_UnclosedStringErrors(t *testing.T) {
-	assertSplitErr(t, "SELECT 'abc;")
-}
-
-func TestSplitSqlStatement_UnclosedCommentErrors(t *testing.T) {
-	assertSplitErr(t, "SELECT 1; /* unclosed")
-}
-
-func TestSplitSqlStatement_Unicode(t *testing.T) {
-	assertSplit(t, "SELECT '你好;世界';", []string{"SELECT '你好;世界'"})
-}
-
-func assertSplit(t *testing.T, sql string, want []string) {
-	t.Helper()
-	got, err := parser.SplitSqlStatement(sql)
-	require.NoError(t, err)
-	assert.Equal(t, want, got)
-}
-
-func assertSplitErr(t *testing.T, sql string) {
-	t.Helper()
-	_, err := parser.SplitSqlStatement(sql)
-	require.Error(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parser.SplitSqlStatements(tt.sql)
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
