945ed7050dd2e1f5b0713479259265a380678448
Balaji J <j.balaji2468@gmail.com>
2026-05-28 16:00:48 +0530

    use key in cmd.cmd to avoid duplicate commands
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- pgxspecial/export.go
+++ pgxspecial/export.go
@@ -3,8 +3,10 @@ package pgxspecial
 // Export returns a list of all registered commands.
 func Export() []CommandExport {
 	var cmds []CommandExport
-	for _, cmd := range commandRegistry {
-		cmd := New(cmd.Cmd, cmd.Syntax, cmd.Description)
+	for key, cmd := range commandRegistry {
+		// key contains the command name with the alias
+		// \quit, '\q, \exit has the same command, only differs by key/alias
+		cmd := New(key, cmd.Syntax, cmd.Description) 
 		cmds = append(cmds, cmd)
 	}
 	return cmds
