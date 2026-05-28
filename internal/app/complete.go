6f013359e7620efd3aa11394490fe65b43af0410
Balaji J <j.balaji2468@gmail.com>
2026-05-28 16:03:29 +0530

    add slash command completions
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/app/complete.go
+++ internal/app/complete.go
@@ -6,9 +6,13 @@ import (
 
 	"github.com/Balaji01-4D/bubbline"
 	"github.com/Balaji01-4D/bubbline/computil"
+	"github.com/Balaji01-4D/bubbline/editline"
+	"github.com/balajz/pgxcli/pgxspecial"
 	"github.com/balajz/pgxls/pkg/engine"
 	"github.com/balajz/pgxls/pkg/types"
 )
+
+const maxCompletions = 8
 
 func (p *pgxCLI) getCompletions() bubbline.AutoCompleteFn {
 	go func() {
@@ -23,15 +27,21 @@ func (p *pgxCLI) getCompletions() bubbline.AutoCompleteFn {
 	compEngine := engine.NewCompleter(p.compWorker.Cache())
 
 	return func(v [][]rune, line, col int) (msg string, comps bubbline.Completions) {
-		compEngine.DBCache = p.compWorker.Cache()
+
 		sql, _ := computil.Flatten(v, line, col)
+		word, wstart, wend := computil.FindWord(v, line, col)
+
+
+		if strings.HasPrefix(word, "\\") && strings.Index(sql, "\\") == 0 {
+			return completeMetaCommand(word, col, wstart, wend, maxCompletions)
+		}
+
+		compEngine.DBCache = p.compWorker.Cache()
 
 		items, err := compEngine.Complete(sql, line, col, true)
 		if err != nil || len(items) == 0 {
 			return "", nil
 		}
-
-		word, wstart, wend := computil.FindWord(v, line, col)
 
 		if lastDot := strings.LastIndex(word, "."); lastDot != -1 {
 			wstart += lastDot + 1
@@ -76,7 +86,7 @@ func (p *pgxCLI) getCompletions() bubbline.AutoCompleteFn {
 			return msg, comps
 		}
 
-		limitKeyword(compByCategory, 8)
+		limitCandidate(compByCategory, maxCompletions)
 
 		categories := make([]string, 0, len(compByCategory))
 		for k := range compByCategory {
@@ -89,6 +99,34 @@ func (p *pgxCLI) getCompletions() bubbline.AutoCompleteFn {
 		}
 		return msg, comps
 	}
+}
+
+func completeMetaCommand(s string, col, wStart, wEnd , limit int) (string, bubbline.Completions) {
+	cmds := pgxspecial.Export()
+
+	var matches struct {
+		cmds []string
+		desc []string
+	}
+
+	for _, cmd := range cmds {
+		if strings.HasPrefix(cmd.Cmd, s) {
+			matches.cmds = append(matches.cmds, cmd.Cmd)
+			matches.desc = append(matches.desc, cmd.Description)
+		}
+
+		if len(matches.cmds) >= limit {
+			break
+		}
+	}
+
+	if len(matches.cmds) == 0 {
+		return "", nil
+	}
+
+	return "", editline.SimpleWordsCompletionWithDescriptions(
+		matches.cmds, matches.desc, "commands", col, wStart, wEnd,
+	)
 }
 
 // https://github.com/cockroachdb/cockroach/blob/refs/heads/master/pkg/cli/clisqlshell/complete.go
@@ -147,10 +185,10 @@ func (c *compCandidate) DeleteLeft() int { return c.deleteLeft }
 
 func (c *compCandidate) SidePanel() string { return c.docs }
 
-func limitKeyword(comp map[string][]compCandidate, limit int) {
-	if comps, ok := comp["keywords"]; ok {
+func limitCandidate(comp map[string][]compCandidate, limit int) {
+	for category, comps := range comp {
 		if len(comps) > limit {
-			comp["keywords"] = comps[:limit]
+			comp[category] = comps[:limit]
 		}
 	}
 }
