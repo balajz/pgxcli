2230d0a27feda89ce69dc9918efa36b7bcebf325
Balaji J <j.balaji2468@gmail.com>
2026-06-11 08:32:46 +0530

    move and rename clicontext
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/app/context.go
+++ internal/app/context.go
@@ -1,18 +1,17 @@
-// Package cli contains the command-line interface for pgxcli.
-package cli
+// Package app is the application layer.
+package app
 
 import (
-	"github.com/balajz/pgxcli/internal/app"
 	"github.com/balajz/pgxcli/internal/cliio"
 	"github.com/balajz/pgxcli/internal/config"
 	"github.com/balajz/pgxcli/internal/database"
 	"github.com/balajz/pgxcli/internal/logger"
 )
 
-// CliContext holds the dependencies for cli.
-type CliContext struct { //revive:disable suggested context name would be misunderstood to context.Context
-	// config holds the global configuration for the cli
-	config *config.Config
+// AppContext holds the dependencies for the application.
+type AppContext struct { //revive:disable suggested context name would be misunderstood to context.Context
+	// Config holds the global configuration for the cli
+	Config *config.Config
 
 	// Logger is used for logging messages and errors
 	Logger *logger.Logger
@@ -26,5 +25,5 @@ type CliContext struct { //revive:disable suggested context name would be misund
 	// App is the application layer that contains the business logic of pgxcli
 	// App orchestrates the execution of commands and interacts with the database client
 	// printer to perform operations and display results.
-	App app.Application
+	App Application
 }
